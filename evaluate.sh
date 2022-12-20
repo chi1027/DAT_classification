fffd600910b7ae14be371cbe710080b00ab2036b
chi1027 <77982357+chi1027@users.noreply.github.com>
2022-12-20 22:21:29 +0800

    update evaluate

--- evaluate.sh
+++ evaluate.sh
@@ -3,4 +3,4 @@ GPU=$1
 CFG=$2
 CKPT=$3
 
-torchrun --nproc_per_node $GPU --master_port $PORT main.py --cfg $CFG --data-path <path-to-imagenet> --eval --resume $CKPT
+torchrun --nproc_per_node $GPU --master_port $PORT main.py --cfg $CFG --data-path $4 --eval --resume $CKPT
